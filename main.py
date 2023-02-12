f7c0a7c6c281e2f01e3a87e38a3ee1b8a8a4e9ea
Sean-Miningah <miningahsean@gmail.com>
2023-02-12 20:09:47 +0300

    Environment Variable Settings

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from typing import Union 
+from functools import lru_cache
 
 from fastapi import FastAPI
 
+from . import config
+
 app = FastAPI()
+
+@lru_cache()
+def get_settings():
+  return config.Settings()
 
 
 @app.get("/")
